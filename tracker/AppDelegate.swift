f325de4b7e1721d8d1bd5ce699052519bae3a78d
Alexey Rytikov <user.raa@gmail.com>
2016-10-31 01:56:51 +0300

    Добавил пример формирования и отправки JSON'а на https.

--- tracker/AppDelegate.swift
+++ tracker/AppDelegate.swift
@@ -16,9 +16,47 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
 
     var window: UIWindow?
     var locationManager: CLLocationManager!
+    var cloudUrlString = "https://steptracker.tw1.ru/track.php"
     
     func deviceUuid() -> String {
         return (UIDevice.current.identifierForVendor?.uuidString)!
+    }
+    
+    func sendJson(urlString: String, data: Any) -> Bool {
+        var jsonData : Data
+        do {
+            jsonData = try JSONSerialization.data(withJSONObject: data, options: JSONSerialization.WritingOptions.prettyPrinted)
+        } catch {
+            print("json error")
+            return false
+        }
+
+        let url = URL(string: urlString)!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        request.httpBody = jsonData
+
+        var httpError = false
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                print("error=\(error)")
+                httpError = true
+                return
+            }
+
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(response)")
+                httpError = true
+            }
+
+            let responseString = String(data: data, encoding: .utf8)
+            print("responseString = \(responseString)")
+        }
+        task.resume()
+
+        return !httpError
     }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
@@ -36,6 +74,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestAlwaysAuthorization()
         locationManager.startUpdatingLocation()
+        
+        print(sendJson(urlString: cloudUrlString, data: [1]))
 
         return true
     }
