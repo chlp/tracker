3e951218e2be51a0fae1f9602449504cb317b3f7
Alexey Rytikov <user.raa@gmail.com>
2016-10-31 00:38:23 +0300

    Убрал лишние комментарии. Вытащил id и параметры устройства с батарею. Подготовил массив данных для отправки.

--- tracker/AppDelegate.swift
+++ tracker/AppDelegate.swift
@@ -9,7 +9,6 @@
 import UIKit
 import Darwin
 import CoreLocation
-//import MapKit
 
 
 @UIApplicationMain
@@ -17,15 +16,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
 
     var window: UIWindow?
     var locationManager: CLLocationManager!
-//    @IBOutlet weak var map: MKMapView!
+    
+    func deviceUuid() -> String {
+        return (UIDevice.current.identifierForVendor?.uuidString)!
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         print("application")
-        
+
         if (!CLLocationManager.locationServicesEnabled()) {
             print("No location manager. Exit")
             exit(0)
         }
+
+        UIDevice.current.isBatteryMonitoringEnabled = true
 
         locationManager = CLLocationManager()
         locationManager.delegate = self
@@ -37,41 +41,41 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        print("locationManager")
         let location = locations.last! as CLLocation
-        print(location)
-//        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-//        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
-//        self.map.setRegion(region, animated: true)
+
+        print(
+            deviceUuid(),
+            location.coordinate.latitude,
+            location.coordinate.longitude,
+            location.speed,
+            UIDevice.current.batteryState,
+            UIDevice.current.batteryLevel,
+            UIDevice.current.model,
+            UIDevice.current.localizedModel,
+            UIDevice.current.name,
+            UIDevice.current.systemName,
+            UIDevice.current.systemVersion
+        )
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
         print("applicationWillResignActive")
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         print("applicationDidEnterBackground")
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         print("applicationWillEnterForeground")
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         print("applicationDidBecomeActive")
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         print("applicationWillTerminate")
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
 
